e51aee9bc055fc27db0ee5f854470e618165a8f0
Anitamonteiro <120335617+Anitamonteiro@users.noreply.github.com>
2025-07-04 21:30:17 -0300

    Update rotas.js

--- rotas/rotas.js
+++ rotas/rotas.js
@@ -1,1 +1,16 @@
+const express = require('express');
+const tarefas = require("../Controllers/tarefas");
 
+const rotas = express();
+
+//Tarefas
+rotas.get('/tarefas', tarefas.listarTarefas);
+rotas.get('/tarefa/:id', tarefas.obterTarefa_id);
+rotas.get('/tarefas/:status', tarefas.filtrarTarefa_status);
+rotas.post('/tarefa',tarefas.criarTarefa);
+rotas.put('/tarefa/:id', tarefas.atualizarTarefa);
+rotas.delete('/tarefa/:id', tarefas.deletarTarefa);
+
+
+
+module.exports = rotas;
